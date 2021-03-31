e2d9c030e4b2d2c81f47dc12b83ba2417254bf3a
Dan Gilmore <dan@dangilmore.com>
2021-03-31 18:33:38 -0400

    Made more changes

--- js/designer.js
+++ js/designer.js
@@ -1,13 +1,13 @@
 import BoardDesign from "./BoardDesign.js";
 
-const defaultDesign = new BoardDesign(
+let customDesign = new BoardDesign(
 	12,
 	16,
 	"1W 1M 1W 1M 1W 1M 1W 1M 1W 1M 1W 1M",
 	false,
 	false
 );
-console.log(defaultDesign);
+
 window.addEventListener("load", eventWindowLoaded, false);
 
 let Debugger = function() { };
@@ -21,18 +21,21 @@ Debugger.log = function(message) {
 }
 
 function eventWindowLoaded() {
-	canvasApp();
+	canvasApp( customDesign );
 };
 
-function canvasApp() {
-
+function canvasApp( ) {
+    let pattern = customDesign.pattern;
+    let patternArray = pattern.split(" ");
+    let arrayLength = patternArray.length;
+    
 	let theCanvas = document.getElementById("previewCanvas");
 	let context = theCanvas.getContext("2d");
-	
+
 	Debugger.log("Drawing Board Design");
 
-	function drawBoardDesign() {
-		
+	function drawBoardDesign( ) {
+
 		context.fillStyle = "#dfdede";
 		context.fillRect(0,0, 500, 300);
 		
@@ -47,7 +50,13 @@ function canvasApp() {
 		imgPadauk.src = "img/padauk-vertical.jpg";
 		imgPurpleHeart.src = "img/purpleheart-vertical.jpg";
 		imgWalnut.src = "img/walnut-vertical.jpg";
-		
+
+		for (var i = 0; i < arrayLength; i++ ) {
+            //console.log( patternArray[i]);
+            let measurement = patternArray[i].charAt(0);
+            let species = patternArray[i].charAt(1);
+        }
+
 		imgWalnut.onload = function() {
 			context.drawImage(imgWalnut, 0, 0, 500, 350/12);
 		};
@@ -70,9 +79,7 @@ jQuery(document).ready( function (){
     let patternArray = pattern.split(" ");
     let arrayLength = patternArray.length;
     
-	for (var i = 0; i < arrayLength; i++ ) {
-        //jQuery("#preview").append('<div class="row"><div class="strip">' + patternArray[i] + '</div></div>');
-    }
+	
 	
 	//TODO: Add validation functionality here. Pattern must be [0-9][A-Z][" "] (AlphaNumber then space)'
 	//      This can be done with the keypress & paste events
