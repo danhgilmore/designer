11f356b41b16e9b67177ef6dd9caf0aa2924541c
danhgilmore <dan@dangilmore.com>
2021-03-31 13:36:13 -0400

    Added BoardDesign.js

---
+++ js/BoardDesign.js
@@ -0,0 +1,32 @@
+﻿class BoardDesign {
+	constructor(
+		// Defines parameters:
+		width,
+		length,
+		pattern,
+		juiceGroove,
+		feet
+	) {
+		// Define the properties:
+		this.width = width;
+		this.length = length;
+		this.pattern = pattern;
+		this.juiceGroove = juiceGroove;
+		this.feet = feet;
+	}
+	
+	updateLength(length) {
+		this.length = length;
+	}
+	updateWidth(width) {
+		this.width = width;
+	}
+	toggleJuiceGroove(juiceGrooveStatus) {
+		this.juiceGroove = juiceGrooveStatus;
+	}
+	toggleFeet(feet) {
+		this.feet = feet;
+	}
+}
+
+export default BoardDesign;
