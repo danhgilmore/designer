09aecf6fb44db8f81f079cb5df7da4722a202141
danhgilmore <dan@dangilmore.com>
2021-03-31 13:35:17 -0400

    Updated Mar 31
    Continued developing

--- js/designer.js
+++ js/designer.js
@@ -1,6 +1,16 @@
+import BoardDesign from "./BoardDesign.js";
+
+const defaultDesign = new BoardDesign(
+	12,
+	16,
+	"1W 1M 1W 1M 1W 1M 1W 1M 1W 1M 1W 1M",
+	false,
+	false
+);
+console.log(defaultDesign);
 window.addEventListener("load", eventWindowLoaded, false);
 
-var Debugger = function() { };
+let Debugger = function() { };
 
 Debugger.log = function(message) {
 	try {
@@ -10,66 +20,63 @@ Debugger.log = function(message) {
 	}
 }
 
-
-
-
 function eventWindowLoaded() {
 	canvasApp();
 };
 
 function canvasApp() {
 
-	var theCanvas = document.getElementById("previewCanvas");
-	var context = theCanvas.getContext("2d");
+	let theCanvas = document.getElementById("previewCanvas");
+	let context = theCanvas.getContext("2d");
 	
-	Debugger.log("Drawing Canvas");
+	Debugger.log("Drawing Board Design");
 
-	function drawScreen() {
+	function drawBoardDesign() {
+		
 		context.fillStyle = "#dfdede";
 		context.fillRect(0,0, 500, 300);
 		
-		var testImage = new Image();
-		testImage.src = "img/walnut-vertical.jpg";
-		testImage.onload = function() {
-			context.drawImage(testImage, 0, 0, 500, 350/12);
+		let imgCherry = new Image();
+		let imgMaple = new Image();
+		let imgPadauk = new Image();
+		let imgPurpleHeart = new Image();
+		let imgWalnut = new Image();
+		
+		imgCherry.src = "img/cherry-vertical.jpg";
+		imgMaple.src = "img/maple-vertical.jpg";
+		imgPadauk.src = "img/padauk-vertical.jpg";
+		imgPurpleHeart.src = "img/purpleheart-vertical.jpg";
+		imgWalnut.src = "img/walnut-vertical.jpg";
+		
+		imgWalnut.onload = function() {
+			context.drawImage(imgWalnut, 0, 0, 500, 350/12);
 		};
 		
 	}	
-	drawScreen();
-	
-
-	
+	drawBoardDesign();
 }
-
-
-
-
 
 jQuery(document).ready( function (){
 
-    jQuery('#width').on('change', function() {
+    /*jQuery('#width').on('change', function() {
         jQuery('#widthPreviewDisplay').html(jQuery('#width').val());
     });
 
     jQuery('#length').on('change', function() {
         jQuery('#lengthPreviewDisplay').html(jQuery('#length').val());
     });
-/*
-    var pattern = jQuery('#pattern').val();
-    var patternArray = pattern.split(" ");
-    var arrayLength = patternArray.length;
-    for (var i = 0; i < arrayLength; i++ ) {
-        jQuery("#preview").append('<div class="row"><div class="strip">' + patternArray[i] + '</div></div>');
+*/
+    let pattern = jQuery('#pattern').val();
+    let patternArray = pattern.split(" ");
+    let arrayLength = patternArray.length;
+    
+	for (var i = 0; i < arrayLength; i++ ) {
+        //jQuery("#preview").append('<div class="row"><div class="strip">' + patternArray[i] + '</div></div>');
     }
-*/	
+	
 	//TODO: Add validation functionality here. Pattern must be [0-9][A-Z][" "] (AlphaNumber then space)'
 	//      This can be done with the keypress & paste events
 	jQuery("#pattern").change(function(event) {
 		
 	});
-	
-	
-
 });
-
-
